4fb6635a90ca595f28f7f502e0519c7e4d6106ff
polyfloyd <floyd@polyfloyd.net>
2020-12-12 18:36:03 +0100

    Add labels for the Y axis

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@
 
 import display
 import neopixel
-from math import pi, sin
+from math import pi, sin, sqrt
 from time import sleep
 from .mhz19 import MHZ19
 
@@ -20,7 +20,25 @@ neopixel.enable()
 neopixel.send(bytes(4 * [0]))
 
 
-def draw_history_graph(rect, history):
+def draw_dashed_line(x0, y0, x1, y1, color=0x000000, space=12):
+    vx, vy = x1-x0, y1-y0
+    l2 = sqrt(vx**2 + vy**2)
+    vec_x, vec_y = vx/l2 * space, vy/l2 * space
+
+    lx0, ly0 = x0, y0
+    while True:
+        lx1, ly1 = min(lx0 + vec_x, x1), min(ly0 + vec_y, y1)
+        display.drawLine(
+            int(lx0), int(ly0),
+            int(min(lx0 + vec_x/2, x1)), int(min(ly0 + vec_y/2, y1)),
+            color)
+        if lx1 == x1 and ly1 == y1:
+            break
+        lx0, ly0 = lx1, ly1
+    display.drawPixel(x1, y1, color)
+
+
+def draw_history_graph_plot(rect, history):
     x, y, w, h = rect
 
     if len(history) == 0:
@@ -37,6 +55,21 @@ def draw_history_graph(rect, history):
         display.drawLine(x0, y0, x1, y1, 0x000000)
 
 
+def draw_history_graph(rect, history):
+    x, y, w, h = rect
+
+    txt_h = display.getTextHeight('-')
+    x_label_h = txt_h
+
+    sample_min, sample_max = min(history), max(history)
+    display.drawText(x, y, '%dppm' % sample_min, 0x000000)
+    display.drawText(x, y-txt_h+h-2 - x_label_h, '%dppm' % sample_max, 0x000000)
+    draw_dashed_line(x, y, x+w-1, y, 0x000000)
+    draw_dashed_line(x, y+h-1 - x_label_h, x+w-1, y+h-1 - x_label_h, 0x000000)
+
+    draw_history_graph_plot((x, y, w, h - x_label_h), history)
+
+
 def draw_co2_label(rect, co2):
     x, y, w, h = rect
 
@@ -50,8 +83,8 @@ def draw_co2_label(rect, co2):
 def draw_ui(co2_history):
     w, h = display.size()
     display.drawFill(0xffffff)  # Clear
-    draw_co2_label((0, 0, w, h//2), co2_history[-1])
-    draw_history_graph((0, h//2, w, h//2), co2_history)
+    draw_co2_label((0, 0, w, h//4), co2_history[-1])
+    draw_history_graph((0, h//4, w, h//4*3), co2_history)
     display.flush()
 
 
